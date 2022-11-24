892edf1432193d96f194950b6f0a046744a924d4
krisztian.maurer <krisztian.maurer@metrix.co.hu>
2022-11-24 17:32:02 +0100

    fix: env default

--- lib/env/env.ts
+++ lib/env/env.ts
@@ -2,32 +2,35 @@ import { config } from 'dotenv';
 config();
 
 export class Env {
-    static get(name: string): string {
+    static get<T = string>(name: string, pipe: (val) => T = (val) => val, def: T): T {
         const value = process.env[name];
-        if (!value) {
-            throw new Error(`Env not found: ${name}`);
+        if (!value && !def) {
+            throw new Error(`$[{Env.name}]: environment variable not found: ${name} and has no default value`);
+        } else if (!value && def != null) {
+            return def;
         }
-        return value;
+        return pipe(value);
     }
-    static asString(name: string): string {
-        return this.get(name);
+
+    static asString(name: string, def: string = null): string {
+        return this.get<string>(name, (val) => val, def);
     }
-    static asNumber(name: string): number {
-        return Number.parseInt(this.get(name));
+    static asNumber(name: string, def: number = null): number {
+        return this.get<number>(name, (val) => Number.parseInt(val), def);
     }
-    static asFloat(name: string): number {
-        return Number.parseFloat(this.get(name));
+    static asFloat(name: string, def: number = null): number {
+        return this.get<number>(name, (val) => Number.parseFloat(val), def);
     }
-    static asArray(name): string[] {
-        return this.asArrayOfString(name);
+    static asArray(name: string, def: string[] = null): string[] {
+        return this.asArrayOfString(name, def);
     }
-    static asArrayOfString(name): string[] {
-        return this.get(name).split(',');
+    static asArrayOfString(name: string, def: string[] = null): string[] {
+        return this.get<string[]>(name, (val) => val.split(','), def);
     }
-    static asArrayOfNumber(name: string): number[] {
-        return this.asArrayOfString(name).map((val) => Number.parseInt(val));
+    static asArrayOfNumber(name: string, def: number[] = null): number[] {
+        return this.get<number[]>(name, (val) => val.split(',').map((val) => Number.parseInt(val)), def);
     }
-    static asArrayOfFloat(name: string): number[] {
-        return this.asArrayOfString(name).map((val) => Number.parseFloat(val));
+    static asArrayOfFloat(name: string, def: number[] = null): number[] {
+        return this.get<number[]>(name, (val) => val.split(',').map((val) => Number.parseFloat(val)), def);
     }
 }
